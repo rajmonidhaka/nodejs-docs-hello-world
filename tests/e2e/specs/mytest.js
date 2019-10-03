7ec7b1e6e144c57f5494794582a650f6ccc5064f
Rajmoni <54482431+rajmonidhaka@users.noreply.github.com>
2019-10-02 21:09:23 -0700

    Update mytest.js

--- tests/e2e/specs/mytest.js
+++ tests/e2e/specs/mytest.js
@@ -2,4 +2,7 @@ describe('My First Test', function() {
   it('Visits the Kitchen Sink', function() {
     cy.visit('/')
   })
+  it('looks inside <title> tag', () => {
+    cy.get('body').should('contain', 'Hello World! Howdy Everyone!!!')
+  })
 })
