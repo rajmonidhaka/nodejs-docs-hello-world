5229deb9edfdad925d4d259f5010d7b124f9166f
Rajmoni <54482431+rajmonidhaka@users.noreply.github.com>
2019-10-02 19:29:37 -0700

    Update mytest.js

--- tests/e2e/specs/mytest.js
+++ tests/e2e/specs/mytest.js
@@ -1,5 +1,5 @@
 describe('My First Test', function() {
   it('Visits the Kitchen Sink', function() {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://shnodeapp.azurewebsites.net')
   })
 })
