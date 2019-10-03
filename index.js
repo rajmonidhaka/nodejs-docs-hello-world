f3eee19d9763d98046b3514fc6523f83e2e57730
Rajmoni <54482431+rajmonidhaka@users.noreply.github.com>
2019-10-02 20:05:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.end("Hello World! Howdy Everyone!!!");
 }).listen(process.env.PORT);
